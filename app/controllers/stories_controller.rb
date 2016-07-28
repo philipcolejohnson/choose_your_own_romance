1d5d6c6768278209c530ec96fd0be9186c6b4274
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-28 09:44:48 -0700

    Routes work

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,31 @@
 class StoriesController < ApplicationController
   def new
+    @story = Story.new
+    @story.save
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    @story.update
+    redirect_to :edit
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def index
+    @stories = Story.all
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    redirect_to stories_path
   end
 
 end
